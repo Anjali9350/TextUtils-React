3e9b77e5aa429b90706dca17cfa412e9b4c07964
Anjali9350 <109589743+Anjali9350@users.noreply.github.com>
2024-07-29 11:04:17 +0530

    Updates

---
+++ precache-manifest.a3022b3b75a4701966c8a3fc05bd8bf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ac4294c5d3cdeb5c82de9d4d257882",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "3ab84bd10e13818fecef",
+    "url": "/TextUtils-React/static/css/main.f0cb387e.chunk.css"
+  },
+  {
+    "revision": "0da3a3a7caac8e2de5c7",
+    "url": "/TextUtils-React/static/js/2.a5b0c296.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils-React/static/js/2.a5b0c296.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4c22086003eed385c50",
+    "url": "/TextUtils-React/static/js/3.b5bd36bd.chunk.js"
+  },
+  {
+    "revision": "3ab84bd10e13818fecef",
+    "url": "/TextUtils-React/static/js/main.c115d8e5.chunk.js"
+  },
+  {
+    "revision": "3a1e738ab66c1a279927",
+    "url": "/TextUtils-React/static/js/runtime-main.05136a7b.js"
+  }
+]);
